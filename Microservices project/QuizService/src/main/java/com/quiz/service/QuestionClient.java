2aee36082ea3eca1e1527b5a7e5537b665796435
Rahulprajapati200 <108113379+Rahulprajapati200@users.noreply.github.com>
2024-04-03 11:05:12 +0530

    Add files via upload

---
+++ Microservices project/QuizService/src/main/java/com/quiz/service/QuestionClient.java
@@ -0,0 +1,21 @@
+package com.quiz.service;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.quiz.entities.Question;
+
+@FeignClient(name = "QUESTIONSERVICE")
+public interface QuestionClient {
+	
+	
+	@GetMapping("/question/quiz/{quizId}")
+	List<Question> getQuestionOfQuiz(@PathVariable Long quizId);
+
+	
+	
+
+}
